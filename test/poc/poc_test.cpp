2dcbef60d9f802bd819a7786c079fd2b2affd2b2
Doug Livesey <biot023@gmail.com>
2012-05-02 01:13:41 +0100

    Trying to work the POC to handle the kinds of mocks I need

--- test/poc/poc_test.cpp
+++ test/poc/poc_test.cpp
@@ -1,6 +1,7 @@
 #include "gmock/gmock.h"
 #include <iostream>
 #include <string>
+#include <memory>
 
 /* Compile:
  * g++-mp-4.7 -c -std=gnu++11 -dynamic -Wall -g -Iinclude -Igmock/include -Igmock/gtest/include -I/usr/local/include -o test/poc/poc_test.o test/poc/poc_test.cpp -I/opt/local/include/ImageMagick   -I/opt/local/include/ImageMagick   -L/opt/local/lib -lMagick++ -lMagickCore   -L/opt/local/lib -lMagick++ -lMagickCore
@@ -21,11 +22,17 @@
 
 namespace poc {
   using namespace std;
-  
-  struct Poc1
+
+  struct IPoc1
   {
+    virtual ~IPoc1() {}
+    virtual const string get_str() const = 0;
+  };
+  
+  struct Poc1 : public IPoc1
+  {
+    Poc1() : _msg( "Default Message" ) {}
     Poc1( const string &msg ) : _msg( msg ) {}
-    virtual ~Poc1() {}
     virtual const string get_str() const { return _msg; }
   private:
     string _msg;
@@ -33,11 +40,11 @@ namespace poc {
 
   struct Poc2
   {
-    Poc2( Poc1 &poc1 ) : _poc1( poc1 ) {}
-    virtual ~Poc2() {}
-    virtual const string get_str() const { return _poc1.get_str(); }
+    virtual const string get_str() const { return _get_poc1()->get_str(); }
   private:
-    Poc1 _poc1;
+    virtual shared_ptr<const IPoc1> _get_poc1() const {
+      return make_shared<const Poc1>();
+    }
   };
   
 }
@@ -46,37 +53,51 @@ namespace {
   using namespace std;
   using namespace poc;
   using ::testing::Test;
-  using ::testing::AtLeast;
-  using ::testing::_;
-  using ::testing::MatcherCast;
-  using ::testing::SafeMatcherCast;
   using ::testing::Return;
+  using ::testing::Invoke;
+  using ::testing::Const;
+  using ::testing::Pointee;
 
-  struct MockPoc1 : public Poc1
+  struct MockPoc1 : public IPoc1
   {
-    MockPoc1() : Poc1( "poc1 string" ) {}
     MOCK_CONST_METHOD0( get_str, const string() );
+  };
+
+  struct MockPoc2 : public Poc2
+  {
+    MockPoc2()
+    {
+      ON_CALL( *this, get_str() )
+        .WillByDefault( Invoke( this, &MockPoc2::_pclass_get_str ) );
+    }
+    MOCK_CONST_METHOD0( get_str, const string() );
+    MOCK_CONST_METHOD0( _get_poc1, shared_ptr<const IPoc1>() );
+  private:
+    const string _pclass_get_str() const { return Poc2::get_str(); }
   };
 
   struct PocTest : public Test
   {
   protected:
     string _msg;
-    MockPoc1 _poc1;
-    Poc2 _poc2;
+    shared_ptr<const IPoc1> _poc1;
+    MockPoc2 _poc2;
     PocTest() :
       _msg( "mock poc1 string" ),
-      _poc2( _poc1 )
+      _poc1( make_shared<MockPoc1>() )
     {
-      EXPECT_CALL( _poc1, get_str() )
+      EXPECT_CALL( *_poc1, get_str() )
         .Times( 1 )
         .WillOnce( Return( _msg ) );
+      EXPECT_CALL( _poc2, _get_poc1() )
+        .Times( 1 )
+        .WillOnce( Return( _poc1 ) );
     }
   };
 
-  // Should return the message returned from poc1
+  // Should instantiate a poc1 and return the result of it's get_str() function
   TEST_F( PocTest,
-          ShouldReturnTheMessageReturnedFromPoc1 ) {
+          ShouldInstantiateAPoc1AndReturnTheResultOfItsGetStrFunction ) {
     EXPECT_EQ( _msg, _poc2.get_str() );
   }
 }
